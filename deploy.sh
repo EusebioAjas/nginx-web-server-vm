0c8ffc5eb207b88923cda5c588e1b442ba2e84e9
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-02-21 16:21:18 -0600

    fix ngrok script to update github repository

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-systemctl stop nginx
+sudo systemctl stop nginx
 
 REPOSITORY_URL="https://github.com/EusebioAjas/nginx-web-server-vm.git"
 LOCAL_DIR="$(pwd)"
 
 if [ ! -d "$LOCAL_DIR" ]
 then
-  git clone "$REPOSITORY_URL"
+  sudo git clone "$REPOSITORY_URL"
 else
-  git pull origin master
+  sudo git pull origin master
 fi
 
-systemctl start nginx
+sudo systemctl start nginx
 ngrok http 80
 
